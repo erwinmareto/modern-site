a14415eaf8ebe4b8dda1436218465849667a3423
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2024-04-28 23:14:05 +0700

    fix: fixed background image for benefit cards

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -14,10 +14,10 @@ const Benefits = () => {
           title="Chat Smarter, Not Harder with Brainwave"
         />
 
-        <div className="flex flex-wrap gap-10 mb-10">
+        <div className="grid gap-10 mb-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {benefits.map((item) => (
             <div
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              className="block relative card-bg p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
               style={{
                 backgroundImage: `url(${item.backgroundUrl})`,
               }}
